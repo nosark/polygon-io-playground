7afda95fbe2c989fc915898ec495a6aba0b345d2
Kyle Nosar <kylenosar@gmail.com>
2022-10-15 14:49:09 -0700

    added error checking / optional fields

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 extern crate dotenv;
 
 use reqwest::Client;
-use serde_json;
 use serde::{Serialize, Deserialize};
-use rust_decimal_macros::dec;
 use rust_decimal::prelude::*;
 
 
@@ -19,11 +17,12 @@ struct Trade {
 
 
 #[derive(Deserialize, Debug)]
-struct Response {
+struct ParsedResponse {
     results: Vec<Trade>,
     status: String,
     request_id: String,
-    next_url: String
+    next_url: Option<String>,
+    previous_url: Option<String>
 }
 
 #[tokio::main]
@@ -36,11 +35,26 @@ async fn main() -> Result<(), reqwest::Error> {
     let complete_url = format!("{}{}?apiKey={}", base_url, coin_type, polygon_api_key);
     let res = reqwest_client.get(complete_url.clone())
         .send()
-        .await?
-        .json::<Response>()
         .await?;
 
-    println!("complete url {:?}" , complete_url.clone());
-    println!("response: {:?}", res);
+    match res.status() {
+        reqwest::StatusCode::OK => {
+            //success process data
+            let parsed_res = res.json::<ParsedResponse>().await?;
+            println!("response: {:?}", parsed_res.results[0]);
+        },
+
+        reqwest::StatusCode::UNAUTHORIZED => {
+            // either auth token subscription expired or dotenv failed to parse
+            println!("Failed to authorize! Please make sure API Key is valid!");
+        },
+
+        _ => {
+            // something is broken scream fire and look for extinguisher...
+            panic!("PANIC : Something is clearly broken, lets investigate!");
+        },
+    }
+
+
     Ok(())
 }
