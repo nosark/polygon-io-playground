429799d378a9e711c8bcc9a76c9f191790c7b4f1
Kyle Nosar <kylenosar@gmail.com>
2022-10-16 16:44:48 -0700

    get request refacto + code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 extern crate dotenv;
 
 use reqwest::Client;
-use serde::{Serialize, Deserialize};
 use rust_decimal::prelude::*;
-
+use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 struct Trade {
@@ -12,49 +11,93 @@ struct Trade {
     id: String,
     participant_timestamp: i64,
     price: Decimal,
-    size: Decimal
+    size: Decimal,
 }
 
-
 #[derive(Deserialize, Debug)]
-struct ParsedResponse {
+struct PolygonResponse {
     results: Vec<Trade>,
     status: String,
     request_id: String,
     next_url: Option<String>,
-    previous_url: Option<String>
+    previous_url: Option<String>,
+}
+
+/// This function will parse trades from our PolygonResponse from
+/// https://api.polygon.io/v3/trades/
+/// and take all the trades and put them into a shared Vec<Trade> that can be read and produce
+/// output by mutiple threads.
+fn parse_response_for_trades(res: &PolygonResponse) -> Vec<Trade> {
+    unimplemented!()
+}
+
+/// This function is used to make asynchronous requests to specified Polygon.io API endpoints
+/// for query variables that are not being used , fill with and empty "" for the formatter
+///
+/// an example query for bitcoin would appear as follows:
+///
+///     request_trade_data(
+///         &some_client,
+///         "https://api.polygon.io/v3/trades/",
+///         "X:BTC-USD",
+///         "",
+///         "",
+///         "",
+///         api_key
+///     )?;
+///
+///
+///
+/// This call is made using no attributes for order, limit, or sort. This is a generic
+/// request. This will return either Ok(PolygonResponse) or an Err().
+async fn request_trade_data(
+    client: &reqwest::Client,
+    base_url: &str,
+    coin_type: &str,
+    order: &str,
+    limit: &str,
+    sort: &str,
+    api_key: String,
+) -> Result<PolygonResponse, reqwest::Error> {
+    let full_url = format!(
+        "{}{}{}{}{}?apiKey={}",
+        base_url, coin_type, order, limit, sort, api_key
+    );
+
+    // after formatting string make request
+    // if request is successful Deserialize data and return the full response.
+    let res = client.get(full_url).send().await?;
+
+    match res.error_for_status() {
+        Ok(res) => {
+            let deserialized_data = res.json::<PolygonResponse>().await?;
+            return Ok(deserialized_data);
+        }
+        Err(err) => {
+            println!("Error: {}", err);
+            return Err(err);
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    dotenv::from_filename("../config.env").ok();
+    dotenv::from_filename("config.env").ok();
     let reqwest_client = Client::new();
     let coin_type = "X:BTC-USD";
-    let base_url: String = "https://api.polygon.io/v3/trades/".to_owned();
     let polygon_api_key = std::env::var("POLYGON_API_KEY").expect("dotenv broke again... WTF");
-    let complete_url = format!("{}{}?apiKey={}", base_url, coin_type, polygon_api_key);
-    let res = reqwest_client.get(complete_url.clone())
-        .send()
-        .await?;
+    let deser_response = request_trade_data(
+        &reqwest_client,
+        "https://api.polygon.io/v3/trades/",
+        coin_type,
+        "",
+        "",
+        "",
+        polygon_api_key,
+    )
+    .await?;
 
-    match res.status() {
-        reqwest::StatusCode::OK => {
-            //success process data
-            let parsed_res = res.json::<ParsedResponse>().await?;
-            println!("response: {:?}", parsed_res.results[0]);
-        },
-
-        reqwest::StatusCode::UNAUTHORIZED => {
-            // either auth token subscription expired or dotenv failed to parse
-            println!("Failed to authorize! Please make sure API Key is valid!");
-        },
-
-        _ => {
-            // something is broken scream fire and look for extinguisher...
-            panic!("PANIC : Something is clearly broken, lets investigate!");
-        },
-    }
-
+    println!("{:?}", deser_response.results[0]);
 
     Ok(())
 }
