4ea7f7c072a32fd6fbf7888ba3cc0c4b6e8f16bb
Kyle Nosar <kylenosar@gmail.com>
2022-10-26 16:09:35 -0700

    forgot to run cargo fmt

--- src/crypto_data.rs
+++ src/crypto_data.rs
@@ -167,14 +167,35 @@ async fn get_next_page_data(
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn test_create_candle_from_trades() {
-        println!("Starting candle test...");        
-        let sample_trades: Vec<Trade> = vec![ 
-            Trade { conditions: vec![1], exchange: 1, id: Some(String::from("123")), participant_timestamp: 9237019287301928370, price: Decimal::from(1), size: Decimal::from(1)},
-            Trade { conditions: vec![1], exchange: 1, id: Some(String::from("124")), participant_timestamp: 9237019287301928380, price: Decimal::from(2), size: Decimal::from(1)},
-            Trade { conditions: vec![1], exchange: 1, id: Some(String::from("125")), participant_timestamp: 9237019287301928390, price: Decimal::from(3), size: Decimal::from(1)}
+        println!("Starting candle test...");
+        let sample_trades: Vec<Trade> = vec![
+            Trade {
+                conditions: vec![1],
+                exchange: 1,
+                id: Some(String::from("123")),
+                participant_timestamp: 9237019287301928370,
+                price: Decimal::from(1),
+                size: Decimal::from(1),
+            },
+            Trade {
+                conditions: vec![1],
+                exchange: 1,
+                id: Some(String::from("124")),
+                participant_timestamp: 9237019287301928380,
+                price: Decimal::from(2),
+                size: Decimal::from(1),
+            },
+            Trade {
+                conditions: vec![1],
+                exchange: 1,
+                id: Some(String::from("125")),
+                participant_timestamp: 9237019287301928390,
+                price: Decimal::from(3),
+                size: Decimal::from(1),
+            },
         ];
         let test_candle = create_candle_from_trades(sample_trades);
         println!("{:?}", test_candle);
@@ -186,7 +207,7 @@ mod tests {
     }
 
     #[actix_rt::test]
-    async fn test_get_trades_for_trading_window() -> Result<(), reqwest::Error> { 
+    async fn test_get_trades_for_trading_window() -> Result<(), reqwest::Error> {
         dotenv::from_filename("config.env").ok();
         let client = reqwest::Client::new();
 
@@ -201,10 +222,10 @@ mod tests {
             sort: "",
         };
 
-        let res: PolygonResponse = request_trade_data(&client, query_params, &polygon_api_key)
-            .await?;
+        let res: PolygonResponse =
+            request_trade_data(&client, query_params, &polygon_api_key).await?;
         let trades = get_trades_for_trading_window(30, res);
-        assert_eq!(trades.len(), 125); 
+        assert_eq!(trades.len(), 125);
         Ok(())
     }
 }
